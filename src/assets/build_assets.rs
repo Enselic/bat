a33b4efd13baaafe4166dbe736ab801717319379
Martin Nordholts <enselic@gmail.com>
2021-12-04 19:50:56 +0100

    wip

--- src/assets/build_assets.rs
+++ src/assets/build_assets.rs
@@ -18,9 +18,11 @@ pub fn build(
 
     let syntax_set = syntax_set_builder.build();
 
+    let acknowledgements = build_acknowledgements(source_dir, include_integrated_assets)?;
+
     print_unlinked_contexts(&syntax_set);
 
-    write_assets(&theme_set, &syntax_set, target_dir, current_version)
+    write_assets(&theme_set, &syntax_set, &acknowledgements, target_dir, current_version)
 }
 
 fn build_theme_set(source_dir: &Path, include_integrated_assets: bool) -> ThemeSet {
@@ -133,6 +135,7 @@ fn print_unlinked_contexts(syntax_set: &SyntaxSet) {
 fn write_assets(
     theme_set: &ThemeSet,
     syntax_set: &SyntaxSet,
+    acknowledgements: Option<&str>,
     target_dir: &Path,
     current_version: &str,
 ) -> Result<()> {
@@ -149,6 +152,14 @@ fn write_assets(
         "syntax set",
         COMPRESS_SYNTAXES,
     )?;
+    if let Some(acknowledgements) = acknowledgements {
+        asset_to_cache(
+            syntax_set,
+            &target_dir.join("acknowledgements.bin"),
+            "acknowledgements",
+            true,
+        )?;
+    }
 
     print!(
         "Writing metadata to folder {} ... ",
