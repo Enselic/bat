adc3cad44424be4cfeb8fee8099b17aab8af4a5c
Martin Nordholts <enselic@gmail.com>
2021-12-05 11:12:00 +0100

    wip

--- src/assets/build_assets/acknowledgements.rs
+++ src/assets/build_assets/acknowledgements.rs
@@ -2,33 +2,34 @@ use std::path::Path;
 
 use crate::error::*;
 
-pub fn build(source_dir: &Path, include_integrated_assets: bool) -> Result<Option<String>> {
-    // TODO: Special flag --build-acknowledgements?
-    if include_integrated_assets {
-        return Ok(None);
-    }
-
-    // Sourced from License section in README.md
-    let preamble = "Copyright (c) 2018-2021 bat-developers (https://github.com/sharkdp/bat).
+// Sourced from License section in README.md
+const preamble: &str = "Copyright (c) 2018-2021 bat-developers (https://github.com/sharkdp/bat).
 
 bat is made available under the terms of either the MIT License or the Apache License 2.0, at your option.
 
 See the LICENSE-APACHE and LICENSE-MIT files for license details.
 ";
 
+pub fn build(source_dir: &Path, include_integrated_assets: bool) -> Result<Option<String>> {
+    // TODO: Special flag --build-acknowledgements?
+    if include_integrated_assets {
+        return Ok(None);
+    }
+
     let mut acknowledgements = String::new();
     acknowledgements.push_str(preamble);
 
     // Sort entries so the order is stable over time
-    let entries =
-        walkdir::WalkDir::new(source_dir).sort_by(|a, b| a.file_name().cmp(b.file_name()));
+    let entries = walkdir::WalkDir::new(source_dir).sort_by(|a, b| a.path().cmp(b.path()));
     for entry in entries {
         let path = entry.map(|e| e.path()).map_err(|e| format!("{}", e))?;
 
-        let stem = match path.file_stem().map(|s| s.to_str()) {
-            Some(stem) => handle_file(path, stem)?,
+        let stem = match path.file_stem().and_then(|s| s.to_str()) {
+            Some(stem) => stem,
             None => continue,
         };
+
+        handle_file(&mut acknowledgements, path, stem)?
     }
 
     return Ok(Some(acknowledgements));
